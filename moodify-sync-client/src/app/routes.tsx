116b18e25cd7f48da1352d9eb8031455e47e10f2
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-05 14:29:14 +0530

    Update routes.tsx

--- moodify-sync-client/src/app/routes.tsx
+++ moodify-sync-client/src/app/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router";
-import Login from "../features/auth/page/Login";
-import Register from "../features/auth/page/register";
 import MainLayout from "../components/layout/MainLayout";
+import Login from "../features/auth/page/Login";
+import Register from "../features/auth/page/Register";
 
 export const router = createBrowserRouter([
   {
