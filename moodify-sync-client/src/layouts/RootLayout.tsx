76553025834b9c2a5b44536de376b5f422b0506e
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-07 20:11:13 +0530

    Update RootLayout.tsx

--- moodify-sync-client/src/layouts/RootLayout.tsx
+++ moodify-sync-client/src/layouts/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const RootLayout = () => {
   return (
     <>
-      <div className="min-h-screen bg-black text-white">
+      <div className="min-h-screen bg-linear-to-bl from-violet-900 to-fuchsia-900 ">
         <Outlet />
       </div>
     </>
