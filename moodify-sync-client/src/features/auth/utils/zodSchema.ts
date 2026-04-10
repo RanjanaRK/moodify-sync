67839b76760514231fed52603cbce011d5ec97d6
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-10 18:25:52 +0530

    schema

---
+++ moodify-sync-client/src/features/auth/utils/zodSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .min(1, "Password is required"),
+});
+
+export const registerSchema = z.object({
+  fullName: z
+    .string()
+    .min(1, "Full name is required")
+    .min(2, "Full name must be at least 2 characters")
+    .max(50, "Full name must not exceed 50 characters"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .min(1, "Password is required"),
+});
