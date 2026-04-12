466bd24acdebb98f273d671d965a7047db68c248
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-12 19:56:25 +0530

    Add .js to env import and simplify route handler
    Update import in db.ts to include the .js extension for ESM resolution when running compiled code. Also simplify the Express root route handler in server.ts by removing explicit Request/Response type annotations from the callback parameters.

--- moodify-sync-server/src/server.ts
+++ moodify-sync-server/src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("server is running");
 });
 
