8f131ac11111e44e383023e9e1a38b7f87523ad1
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-24 00:06:11 +0530

    Update index.ts

--- moodify-sync-server/src/index.ts
+++ moodify-sync-server/src/index.ts
@@ -13,7 +13,7 @@ app.set("trust proxy", 1);
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE"],
   }),
 );
@@ -24,6 +24,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/song", songRouter);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
   console.log("server is running okay");
