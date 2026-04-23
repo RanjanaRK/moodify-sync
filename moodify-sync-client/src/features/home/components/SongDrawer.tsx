6c1920ad0724284e6655a008facef4925633b484
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-23 16:45:31 +0530

    Update SongDrawer.tsx

--- moodify-sync-client/src/features/home/components/SongDrawer.tsx
+++ moodify-sync-client/src/features/home/components/SongDrawer.tsx
@@ -12,15 +12,15 @@ const SongDrawer = ({ songs, onSelect, currentSongId }: Props) => {
 
   return (
     <>
-      {/* Toggle Button */}
-      <button
-        onClick={() => setOpen(!open)}
-        className="fixed top-6 right-6 z-50 px-4 py-2 bg-white text-black rounded-full shadow-lg"
-      >
-        🎵 Songs
-      </button>
+      <div className="pt-24 border-4 mt-24">
+        <button
+          onClick={() => setOpen(!open)}
+          className=" top-6 right-6  px-4 py-2 bg-white text-black rounded-full shadow-lg"
+        >
+          Songs
+        </button>
+      </div>
 
-      {/* Overlay */}
       {open && (
         <div
           onClick={() => setOpen(false)}
@@ -28,19 +28,16 @@ const SongDrawer = ({ songs, onSelect, currentSongId }: Props) => {
         />
       )}
 
-      {/* Drawer */}
       <div
-        className={`fixed top-0 right-0 h-full w-[360px] bg-[#0b0b0f] text-white z-50 shadow-2xl transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full w-90 bg-[#0b0b0f] text-white z-50 shadow-2xl transform transition-transform duration-300 ${
           open ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        {/* Header */}
         <div className="p-4 border-b border-white/10 flex justify-between items-center">
           <h2 className="text-lg font-semibold">Your Songs</h2>
           <button onClick={() => setOpen(false)}>✖</button>
         </div>
 
-        {/* Song List */}
         <div className="p-3 space-y-3 overflow-y-auto h-full pb-24">
           {songs.map((song) => {
             const active = song.id === currentSongId;
@@ -56,20 +53,17 @@ const SongDrawer = ({ songs, onSelect, currentSongId }: Props) => {
                     : "hover:bg-white/5"
                 }`}
               >
-                {/* Thumbnail */}
                 <img
                   src={song.posterUrl}
                   className="w-12 h-12 rounded-lg object-cover"
                 />
 
-                {/* Info */}
                 <div className="flex-1">
                   <h3 className="text-sm font-medium">{song.title}</h3>
                   <p className="text-xs text-gray-400">{song.mood}</p>
                 </div>
 
-                {/* Play Indicator */}
-                <div className="text-sm">{active ? "🎧" : "▶"}</div>
+                <div className="text-sm">{active ? "play" : "pause"}</div>
               </div>
             );
           })}
