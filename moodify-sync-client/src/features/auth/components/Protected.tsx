bfd4f21c8f4f3c805a7c7844bb8e6cf91f636d3e
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-24 15:38:23 +0530

    Update Protected.tsx

--- moodify-sync-client/src/features/auth/components/Protected.tsx
+++ moodify-sync-client/src/features/auth/components/Protected.tsx
@@ -1,12 +1,19 @@
 import type { ReactNode } from 'react';
 import { Navigate } from 'react-router';
 import { useCurrentUser } from '../hooks/useUser';
+import { Loader2 } from 'lucide-react';
 
 const Protected = ({ children }: { children: ReactNode }) => {
   const { currentUserQuery } = useCurrentUser();
   const user = currentUserQuery.data?.user;
 
-  console.log(user);
+  if (currentUserQuery.isLoading) {
+    return (
+      <>
+        <Loader2 className="animate-spin" />
+      </>
+    );
+  }
 
   if (!user) {
     return <Navigate to="/auth/login" replace />;
