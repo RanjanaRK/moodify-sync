b1ed91bbf9b272ae3a6657aaade994b84e071baa
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-24 15:16:43 +0530

    Update env.ts

--- moodify-sync-server/src/config/env.ts
+++ moodify-sync-server/src/config/env.ts
@@ -2,16 +2,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
+if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
+if (!process.env.REDIS_HOST) throw new Error("REDIS_HOST is not defined");
+if (!process.env.REDIS_PORT) throw new Error("REDIS_PORT is not defined");
+if (!process.env.REDIS_PASSWORD)
+  throw new Error("REDIS_PASSWORD is not defined");
+
 const env = {
-  MONGO_URI: process.env.MONGO_URI || "",
+  MONGO_URI: process.env.MONGO_URI,
 
-  JWT_SECRET: process.env.JWT_SECRET || "",
+  JWT_SECRET: process.env.JWT_SECRET,
 
-  REDIS_HOST: process.env.REDIS_HOST || "",
+  REDIS_HOST: process.env.REDIS_HOST,
 
-  REDIS_PORT: process.env.REDIS_PORT || "",
+  REDIS_PORT: process.env.REDIS_PORT,
 
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD || "",
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
 };
 
 export default env;
